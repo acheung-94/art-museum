76ec328938f3c419ce6afab12900310534c4353a
acheung-94 <acheung@ucdavis.edu>
2024-03-15 16:35:28 -0700

    complete phase 3

--- art-museum/src/App.jsx
+++ art-museum/src/App.jsx
@@ -1,11 +1,40 @@
 import harvardArt from '/src/data/harvardArt.js'
 import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 import GalleryNavigation from './components/GalleryNavigation';
+import {Outlet} from 'react-router-dom'
+
 
 const router = createBrowserRouter( [
-  {path: "*", element: <h2>Page Not Found</h2>},
-  {path: "/galleries", element: < GalleryNavigation galleries={harvardArt.records} />}
+  { element: <Layout />,
+    children: [
+      {
+        path: "/", 
+        element: 
+          <>
+            <h2>Harvard Art Museum</h2>
+            <p>
+              Look, but Don&apos;t Touch. Please select a Gallery in the
+              navigation bar.
+            </p>
+          </>
+      },
+      {path: "*", element: <h2>Page Not Found</h2>}
+
+    ]}
 ])
+
+function Layout() {
+  return (
+    <div className="page-wrapper">
+      <GalleryNavigation galleries={harvardArt.records} />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
+
 
 function App() {
   return < RouterProvider router={router} />;
--- art-museum/src/components/GalleryNavigation/GalleryNavigation.jsx
+++ art-museum/src/components/GalleryNavigation/GalleryNavigation.jsx
@@ -1,15 +1,15 @@
 import { NavLink } from "react-router-dom";
-
+import './GalleryNavigation.css'
 function GalleryNavigation({ galleries }) {
     console.log(galleries);
     return (    
         <nav>
+            <NavLink to='/' > <h1>Galleries Home</h1></NavLink>
+            
             {galleries.map(element => (
                 <NavLink to={`galleries/${element.id}`} key={element.id}>{element.name}</NavLink>
             )) 
             }
-  
-          <h1>Galleries</h1>  
 
         </nav>
     )
