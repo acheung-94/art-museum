2828829187ee9db0f487b5b9dad44715fe06e270
acheung-94 <acheung@ucdavis.edu>
2024-03-15 20:14:04 -0700

    complete phase 4

--- art-museum/src/components/GalleryView/GalleryView.jsx
+++ art-museum/src/components/GalleryView/GalleryView.jsx
@@ -1,11 +1,25 @@
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
+
+function findGallery(galleries, galleryId) {
+    let found = galleries.find( (gallery) => (
+        gallery.galleryid === JSON.parse(galleryId)
+    ))
+    return found
+}
+
 
 function GalleryView( {galleries} ) {
     let { galleryId } = useParams();
-    
-    console.log(galleryId);
     return (
-        <h1>Hello from GalleryView</h1>
+        <>
+            <h1>Hello from GalleryView</h1>
+            
+            { findGallery(galleries, galleryId) ?
+                <h2>{ findGallery(galleries, galleryId).name }</h2> :
+                <Navigate to='/' replace={true}> </Navigate>
+            }
+            
+        </>
     )
 }
 
